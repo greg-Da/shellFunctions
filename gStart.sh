0faa747cea9fc2849f29e6f53dfa49eb227b0ee6
Grégoire Dalmasso <45882753+greg-Da@users.noreply.github.com>
2023-10-31 15:58:46 +0100

    Update gStart.sh

--- gStart.sh
+++ gStart.sh
@@ -1,8 +1,10 @@
-printf 'SSH origin : ';
-read -r varname;
-git init;
-git add .;
-git commit -m 'first commit';
-git branch -M main;
-git remote add origin $varname;
-git push -u origin main;
+gStart () { 
+    printf 'SSH origin : ';
+    read -r varname;
+    git init;
+    git add .;
+    git commit -m 'first commit';
+    git branch -M main;
+    git remote add origin $varname;
+    git push -u origin main;
+}
